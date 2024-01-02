f70aaf75b6cc982aebd9e65262167b04e7549337
Dokolin <dokolin05@mail.ru>
2024-01-02 22:09:23 +0300

    add new req for filter by surname

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ def test_print():
     return 'Hello world!'
 
 
+@app.get('/get_info_all_users')
+def get_info_all():
+    new_session = connect_to_db()
+    info = new_session.query(User).all()
+    new_session.close()
+    return info
+
+
+@app.get('/get_info_with_surname')
+def info_with_surname(surname_filter: str):
+    new_session = connect_to_db()
+    info = new_session.query(User).filter(User.surname == surname_filter).all()
+    new_session.close()
+    return info
+
+
 @app.post('/add_new_user')
 def add_new_user(name: str, surname: str, age: int):
     new_session = connect_to_db()
